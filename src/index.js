3cd441ef26d3314b3f2648a51d2c8e9bbdcfb71e
Zachary Abresch <zachary.abresch@gmail.com>
2018-12-17 02:06:56 +0000

    Implement resursive questioning

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import inquirer from 'inquirer';
 
+const collectExpenses = async (expenses = []) => {
+  const prompts = [
+    { type: 'input', name: 'name', message: 'Expense name: ' },
+    { type: 'input', name: 'amount', message: 'Expense amount: ' },
+    { type: 'input', name: 'due', message: 'Expense due date: ' },
+    { type: 'confirm', name: 'again', message: 'Enter another expense?', default: false }
+  ];
+
+  const { again, ...answers } = await inquirer.prompt(prompts);
+  const newExpenses = [...expenses, answers];
+  return again ? collectExpenses(newExpenses) : newExpenses;
+};
+
 const main = async () => {
   const questions = [
     { type: 'input', name: 'paydays[0]', message: 'What is your first pay day?' },
@@ -8,8 +21,11 @@ const main = async () => {
     { type: 'input', name: 'paychecks[1]', message: 'What is your second paycheck amount?' }
   ];
 
-  const answers = await inquirer.prompt(questions);
-  console.log(answers);
+  const paychecks = await inquirer.prompt(questions);
+  const expenses = await collectExpenses();
+
+  console.log(paychecks);
+  console.log(expenses);
 };
 
 main();
